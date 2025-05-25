5abdacbcb70de7f069e9cb071b6ff5f389ff0939
Tansim Ahmed Tashdid <tansimahmedtasjid@gmail.com>
2025-05-25 15:25:18 +0600

    min , max , swap

--- test.cpp
+++ test.cpp
@@ -40,17 +40,16 @@
 // {
 //     int x;
 
-//     while (cin >> x)// জতখন ইনপুট আছে ততখন প্রিন্ট হবে  
+//     while (cin >> x)// জতখন ইনপুট আছে ততখন প্রিন্ট হবে
 //     {
 //         /* code */
-//         cout << x << endl; 
+//         cout << x << endl;
 //     }
 
 //     return 0;
 // }
 
-
-// setprecision ---> দশমিকের পরে কয় ঘর নিব 
+// setprecision ---> দশমিকের পরে কয় ঘর নিব
 // #include <iostream>
 // #include <iomanip>// input output manipulation
 // using namespace std;
@@ -60,42 +59,74 @@
 //     double x = 3.03393;
 
 //     cout << fixed << setprecision(2) << x ;
-    
+
 //     x > 4 ? cout << "Big" : cout << "Small";
 
 //     return 0;
 // }
 
+// Switch
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int x ;
+//     cin >> x;
+
+//    switch (x)
+//    {
+//    case 1:
+//     /* code */
+//     cout << "Friday";
+//     break;
+//    case 2:
+//     /* code */
+//     cout << "Saturday";
+//     break;
+//    case 3:
+//     /* code */
+//     cout << "Sunday";
+//     break;
+
+//    default:
+//    cout << "Kono din nai";
+//     break;
+
+//    }
+
+//     return 0;
+// }
 
 #include <iostream>
-#include <iomanip>// input output manipulation
+#include <algorithm>
 using namespace std;
 
 int main()
 {
-    int x ;
-    cin >> x;
+    int x, y;
+    cin >> x >> y;
 
-   switch (x)
-   {
-   case 1:
-    /* code */
-    cout << "Friday";
-    break;
-   case 2:
-    /* code */
-    cout << "Saturday";
-    break;
-   case 3:
-    /* code */
-    cout << "Sunday";
-    break;
-   
-   default:
-   cout << "Kono din nai";
-    break;
+    // swap
+    swap(x, y);
 
-   }
+    // min
+    cout << min(x, y) << endl;
+
+    // min multi value
+    cout << min({
+                9,
+                39,
+                94,
+            })
+         << endl;
+
+    // max
+    cout << max(x, y) << endl;
+    // cout << swap(x,y) << endl;
+
+    cout << "X-> " << x << endl
+         << "Y-> " << y;
 
     return 0;
 }
