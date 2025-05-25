90e0c8c7252ecfd2a3ea1b63fcb990cc2c34f2ac
Tansim Ahmed Tashdid <tansimahmedtasjid@gmail.com>
2025-05-25 11:39:38 +0600

    test c++

--- test.cpp
+++ test.cpp
@@ -1,7 +1,65 @@
-#include<iostream>
+// #include<iostream>
 
-int main ()
+// int main ()
+// {
+//     int x = 30;
+//     std:: cout << x << std::endl << " " << "Hello" << " ";
+//     std :: cout << "Allah hu akbar";
+//     return 0;
+// }
+
+// #include<iostream>
+
+// int main ()
+// {
+//     int x ;
+//     std:: cin >> x;
+//     std:: cout << x << std::endl << " " << "Hello" << " ";
+//     std :: cout << "Allah hu akbar";
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int x;
+//     cin >> x;
+//     cout << (char)x << endl // type casting
+//          << " " << "Hello" << " ";
+//     cout << "Allah hu akbar";
+//     return 0;
+// }
+
+// EOF
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int x;
+
+//     while (cin >> x)// জতখন ইনপুট আছে ততখন প্রিন্ট হবে  
+//     {
+//         /* code */
+//         cout << x << endl; 
+//     }
+
+//     return 0;
+// }
+
+
+// setprecision ---> দশমিকের পরে কয় ঘর নিব 
+#include <iostream>
+#include <iomanip>// input output manipulation
+using namespace std;
+
+int main()
 {
-    std :: cout << "Allah hu akbar";
+    double x = 3.03393;
+
+    cout << fixed << setprecision(2) << x ;
+
     return 0;
 }
