5c6a73821f1781124eb7a7689de5fc631776e830
Tansim Ahmed Tashdid <tansimahmedtasjid@gmail.com>
2025-06-14 20:28:07 +0600

    stringstream

--- mod-5.cpp
+++ mod-5.cpp
@@ -2,14 +2,13 @@
 // using namespace std;
 
 // int main() {
-    
+
 //     string s = "Hello World" ;
 //     for(string:: iterator it = s.begin(); it < s.end();it++) {
 //         cout<< *it << endl;
 //     };
 //     return 0;
 // }
-
 
 // #include <bits/stdc++.h>
 // using namespace std;
@@ -26,17 +25,38 @@
 //     return 0;
 // }
 
+// #include <bits/stdc++.h>
+// using namespace std;
 
+// int main() {
+//     int x ;
+//     cin >> x;
+//     cin.ignore(); // enter কে ignore করতে হবে ।
+//     string s ;
+//     getline(cin, s);
+//     cout << x << endl ;
+//     cout << s ;
+//     return 0;
+// }
+
+// !stringstream
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int x ;
-    cin >> x;
-    cin.ignore(); // enter কে ignore করতে হবে । 
-    string s ;
+int main()
+{
+    string s;
     getline(cin, s);
-    cout << x << endl ; 
-    cout << s ; 
+    stringstream ss(s);
+
+    string word;
+
+    while (ss >> word)
+    {
+        /* code */
+        cout << word << endl;
+    }
+
+    // cout << s << endl ;
     return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,15 +1,2 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    int a,b;
-    cin >> a >> b;
-    long long int cal1 = a+b;
-    long long int calc2 = a*b;
-    long long int calc3 = a-b;
-    // 5 + 10 = 15
-    cout << a << " " << "+ " << b << " = " << cal1 << endl;
-    cout << a << " " << "* " << b << " = " << calc2 << endl;
-    cout << a << " " << "- " << b << " = " << calc3;
-    return 0;
-}
+s >> word;
+    cout << word << endl ;
