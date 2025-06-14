cc91ecfe4bf6b356a463d5d3f6dda4c8e9c9b723
Tansim Ahmed Tashdid <tansimahmedtasjid@gmail.com>
2025-06-14 20:05:58 +0600

    string iterator

---
+++ mod-5.cpp
@@ -0,0 +1,23 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+    
+//     string s = "Hello World" ;
+//     for(string:: iterator it = s.begin(); it < s.end();it++) {
+//         cout<< *it << endl;
+//     };
+//     return 0;
+// }
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s = "Hello Worldfe";
+    for(string :: iterator it = s.begin() ; it < s.end();it++){
+        cout << *it << endl;
+    }
+    return 0;
+}
