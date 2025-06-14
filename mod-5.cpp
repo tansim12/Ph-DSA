f328be290aecd282701e43dbac39bff572b44e74
Tansim Ahmed Tashdid <tansimahmedtasjid@gmail.com>
2025-06-14 20:15:21 +0600

    string input

--- mod-5.cpp
+++ mod-5.cpp
@@ -11,17 +11,32 @@
 // }
 
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     string s = "Hello Worldfe";
+//     // for(string :: iterator it = s.begin() ; it < s.end();it++){
+//     //     cout << *it << endl;
+//     // }
+
+//     for(auto  it = s.begin() ; it < s.end() ; it ++){
+//         cout << *it << endl;
+//     }
+//     return 0;
+// }
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    string s = "Hello Worldfe";
-    // for(string :: iterator it = s.begin() ; it < s.end();it++){
-    //     cout << *it << endl;
-    // }
-
-    for(auto  it = s.begin() ; it < s.end() ; it ++){
-        cout << *it << endl;
-    }
+    int x ;
+    cin >> x;
+    cin.ignore(); // enter কে ignore করতে হবে । 
+    string s ;
+    getline(cin, s);
+    cout << x << endl ; 
+    cout << s ; 
     return 0;
 }
