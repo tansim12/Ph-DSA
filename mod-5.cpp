f8552039938917014559d49eff69d79bea775743
Tansim Ahmed Tashdid <tansimahmedtasjid@gmail.com>
2025-06-14 20:07:42 +0600

    string iterator auto

--- mod-5.cpp
+++ mod-5.cpp
@@ -16,7 +16,11 @@ using namespace std;
 
 int main() {
     string s = "Hello Worldfe";
-    for(string :: iterator it = s.begin() ; it < s.end();it++){
+    // for(string :: iterator it = s.begin() ; it < s.end();it++){
+    //     cout << *it << endl;
+    // }
+
+    for(auto  it = s.begin() ; it < s.end() ; it ++){
         cout << *it << endl;
     }
     return 0;
