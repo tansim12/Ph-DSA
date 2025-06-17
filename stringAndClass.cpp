82b595923791dc1d744f478c0dcdad371ee73b1b
Tansim Ahmed Tashdid <tansimahmedtasjid@gmail.com>
2025-06-17 19:47:57 +0600

    different way of call by string constructor

---
+++ stringAndClass.cpp
@@ -0,0 +1,25 @@
+
+//! different way of call by string constructor
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     string s("Hello World",3);
+
+//     // string s("Hello World",3,9);
+
+//     // string s = "Hello world";
+//     // string t(s,3);
+
+//     // string s = "Hello world";
+//     // string t(s,0,3);
+
+//     // string t(59,'H');
+    
+//     cout << s<< endl ;
+//     return 0;
+// }
+
+
+
+
