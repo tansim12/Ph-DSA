506f368df3fc063641ae86e4b66324aa65a77044
Tansim Ahmed Tashdid <tansimahmedtasjid@gmail.com>
2025-06-17 20:12:30 +0600

    reverse word

--- stringAndClass.cpp
+++ stringAndClass.cpp
@@ -15,12 +15,10 @@
 //     // string t(s,0,3);
 
 //     // string t(59,'H');
-    
+
 //     cout << s<< endl ;
 //     return 0;
 // }
-
-
 
 // ! sor by asc and desc
 // #include <bits/stdc++.h>
@@ -30,41 +28,66 @@
 //     string s ;
 //     getline(cin , s);
 
-//     // sort(s.begin(),s.end()); // ASC 
+//     // sort(s.begin(),s.end()); // ASC
 //     sort(s.begin(),s.end(),greater<char>()); // DESC
 //     cout << s << endl ;
 //     return 0;
 // }
 
-//! for range loop 
+//! for range loop
 // #include <bits/stdc++.h>
 // using namespace std;
 
 // int main() {
-    
-//     string s ; 
+
+//     string s ;
 //     getline(cin , s);
 
 //     for (auto &&i : s)
 //     {
 //         cout << i << endl ;
 //     }
-    
+
 //     return 0;
 // }
 
-//! array reverse 
+//! number array reverse
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     int arr[5] = {1,2,3,4,5};
+//     reverse(arr , arr+5);
+
+//     for (auto &&i : arr)
+//     {
+//         cout << i << endl;
+//     }
+
+//     return 0;
+// }
+
+
+// !reverse word 
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int arr[5] = {1,2,3,4,5};
-    reverse(arr , arr+5);
-    
-    for (auto &&i : arr)
+int main()
+{
+    string s;
+    getline(cin, s);
+
+    stringstream ss(s);
+
+    string word;
+
+    while (ss >> word)
     {
-        cout << i << endl;
+        /* code */
+
+        reverse(word.begin(), word.end());
+        cout << word << " ";
     }
-    
+
     return 0;
 }
