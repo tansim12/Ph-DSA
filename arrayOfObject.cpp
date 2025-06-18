004b38407c087107298820327e32c4a797eb9b8f
Tansim Ahmed Tashdid <tansimahmedtasjid@gmail.com>
2025-06-19 00:02:54 +0600

    sort by class student

--- arrayOfObject.cpp
+++ arrayOfObject.cpp
@@ -1,3 +1,53 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// class Student
+// {
+// public:
+//     string name;
+//     int roll;
+// };
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     Student student[n];
+
+//     for (auto &&i : student)
+//     {
+//         cin >> i.name >>  i.roll;
+//     }
+
+//     // ! min and max roll
+//     // int mm = INT_MAX;
+//     // int mx = INT_MIN;
+
+//     // for (auto &&i : student)
+//     // {
+//     //     mm =  min(i.roll,mm);
+//     //     mx =  max(i.roll,mx);
+//     // }
+
+//     // cout << mm << endl << mx  ;
+
+//     Student mn ;
+//     mn.roll = INT_MAX;
+//     for (auto &&i : student)
+//     {
+//        if (i.roll < mn.roll)
+//        {
+//         /* code */
+//         mn = i ;
+//        }
+
+//     }
+
+//      cout << mn.name << " " << mn.roll << endl  ;
+
+//     return 0;
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -8,6 +58,13 @@ public:
     int roll;
 };
 
+//! desc sort
+bool compare(Student l, Student r )
+{
+    // return l.name < r.name;
+   return l.roll > r.roll;
+}
+
 int main()
 {
     int n;
@@ -16,36 +73,15 @@ int main()
 
     for (auto &&i : student)
     {
-        cin >> i.name >>  i.roll;
+        cin >> i.name >> i.roll;
     }
 
+    sort(student, student + n, compare);
 
-    // ! min and max roll 
-    // int mm = INT_MAX;
-    // int mx = INT_MIN;
-
-    // for (auto &&i : student)
-    // {
-    //     mm =  min(i.roll,mm);
-    //     mx =  max(i.roll,mx);
-    // }
-    
-    // cout << mm << endl << mx  ;
-
-
-    Student mn ;
-    mn.roll = INT_MAX;
     for (auto &&i : student)
     {
-       if (i.roll < mn.roll)
-       {
-        /* code */
-        mn = i ;
-       }
-       
+        cout << i.name << " " << i.roll << endl;
     }
 
-     cout << mn.name << " " << mn.roll << endl  ;
-    
     return 0;
 }
