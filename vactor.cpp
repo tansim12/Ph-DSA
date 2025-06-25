05b22b869b6de4f23c8b7019c39b14c4d8421920
Tansim Ahmed Tashdid <tansimahmedtasjid@gmail.com>
2025-06-25 15:34:10 +0600

    vendor input by string

--- vactor.cpp
+++ vactor.cpp
@@ -1,27 +1,93 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     //  vector<int> n ; // type 1
+//     // vector <int>n(10); // type 2
+//     // vector<int> arr(20, 2); // 20 hocche size , 2 hocche sob element
+//     // vector <int> arr = {1,3,3}; // 
+//     // vector <int> arr2(arr); // copy arr
+//     int arr3[3] = {1,2,3};
+//     vector<int> arr2(arr3, arr3 + 3); // copy arr
+
+//     // for (size_t i = 0; i < arr.size(); i++)
+//     // {
+//     //     /* code */
+//     //     cout << arr[i] << " " ;
+//     // }
+
+//     for (size_t i = 0; i < arr2.size(); i++)
+//     {
+//         /* code */
+//         cout << arr2[i] << " ";
+//     }
+
+//     return 0;
+// }
+
+
+//! input vector by array
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     // given size 
+//     int n ;
+//     cin >> n ;
+//     // vector <int> arr(n);
+//     // for (auto &&i : arr)
+//     // {
+//     //     cin >> i ;
+//     //     cout << i << " ";
+//     // }
+
+
+//     // with out size 
+
+
+//     vector <int> arr;
+//     for (size_t i = 0; i < n; i++)
+//     {
+//         /* code */
+//         int x ;
+//         cin >> x ;
+//         arr.push_back(x);
+
+//     }
+//     for (size_t i = 0; i < arr.size(); i++)
+//     {
+        
+//         cout << arr[i] << " " ;
+
+//     }
+    
+
+    
+    
+//     return 0;
+// }
+
+
+
+//! vector input by string 
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    //  vector<int> n ; // type 1
-    // vector <int>n(10); // type 2
-    // vector<int> arr(20, 2); // 20 hocche size , 2 hocche sob element
-    // vector <int> arr = {1,3,3}; // 
-    // vector <int> arr2(arr); // copy arr
-    int arr3[3] = {1,2,3};
-    vector<int> arr2(arr3, arr3 + 3); // copy arr
-
-    // for (size_t i = 0; i < arr.size(); i++)
-    // {
-    //     /* code */
-    //     cout << arr[i] << " " ;
-    // }
-
-    for (size_t i = 0; i < arr2.size(); i++)
+int main() {
+    int n ;
+    cin >> n ;
+    cin.ignore();
+    vector<string> nameArray(n);
+    for (auto &&i : nameArray)
     {
-        /* code */
-        cout << arr2[i] << " ";
+        cin >> i ;
     }
-
+    for (auto &&i : nameArray)
+    {
+        cout << i << endl ;
+    }
+    
+    
     return 0;
 }
