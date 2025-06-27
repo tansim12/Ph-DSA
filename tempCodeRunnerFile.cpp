8438d431057da64278c812a4c7e54d2e8bf2f67e
Tansim Ahmed Tashdid <tansimahmedtasjid@gmail.com>
2025-06-27 21:36:41 +0600

    Equilibrium Index

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,2 +1,23 @@
-s >> word;
-    cout << word << endl ;
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n ; 
+    cin >> n ;
+    vector<int> arr(n);
+    vector<int> prefixArray(n);
+    for (auto &&i : arr)
+    {
+        cin >> i ;
+    }
+    int sum = 0 ;
+    for (size_t i = 0; i < n; i++)
+    {
+        /* code */
+         prefixArray[i] = sum+arr[i];
+        sum +=arr[i];
+        cout << prefixArray[i] << " ";
+    }
+     
+    return 0;
+}
--- week-1.practice.cpp
+++ week-1.practice.cpp
@@ -3,7 +3,7 @@
 // using namespace std;
 
 // int main() {
-//     int n ; 
+//     int n ;
 //     cin >> n ;
 //     vector<int> arr(n);
 //     vector<int> prefixArray(n);
@@ -19,6 +19,46 @@
 //         sum +=arr[i];
 //         cout << prefixArray[i] << " ";
 //     }
-     
+
 //     return 0;
 // }
+
+//!Equilibrium Index 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     vector<int> arr(n);
+//     for (auto &&i : arr)
+//     {
+//         cin >> i;
+//     }
+
+//     for (size_t i = 0; i < n; i++)
+//     {
+//         int sumOfLeft = 0, sumOfRight = 0;
+
+//         //  i এর উপরে depend করে right side loop
+//     for (size_t j = i + 1; j < n; j++)
+//         {
+//             sumOfRight += arr[j];
+//         }
+
+//         //  i এর উপরে depend করে left side loop
+//          for (size_t k = 0; k < i; k++)
+//         {
+//             sumOfLeft += arr[k];
+//         }
+
+//          if (sumOfLeft == sumOfRight)
+//         {
+//             cout  << i << endl;
+//         }
+//     }
+
+//     return 0;
+// }
+
