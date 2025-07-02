0d93753583a4a000f83d4127e2f98f80680486f8
Tansim Ahmed Tashdid <tansimahmedtasjid@gmail.com>
2025-07-02 15:56:24 +0600

    print forword

--- singly-linklist.cpp
+++ singly-linklist.cpp
@@ -25,7 +25,7 @@
 //     return 0;
 // }
 
-//! dynamic node 
+//! dynamic node
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -43,16 +43,26 @@ public:
     }
 };
 
-int main() {
+int main()
+{
 
-    Node* head = new Node(10); // এইগুলা একেক্তা pointer return করে । 
-    Node* a = new Node(20);
-    Node* b = new Node(30);
+    Node *head = new Node(10); // এইগুলা একেক্তা pointer return করে ।
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
 
-    head->next = a ;
-    a->next = b ;
+    head->next = a; // set pointer by next
+    a->next = b;
+    b->next = c;
 
-    cout << a->value ;
-    
+    Node* temp = head; // head কখনো null করা যাবে না । 
+
+    while (temp != NULL)
+    {
+        /* code */
+        cout << temp->value << endl;
+        temp = temp->next;
+    }
+
     return 0;
 }
