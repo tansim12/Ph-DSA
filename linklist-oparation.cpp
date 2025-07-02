216b35342c75e62cde7e6983d94fa4057a791aa4
Tansim Ahmed Tashdid <tansimahmedtasjid@gmail.com>
2025-07-02 20:24:28 +0600

    insert head

---
+++ linklist-oparation.cpp
@@ -0,0 +1,71 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void fn(int* &p)
+// {
+
+//     cout << "fn-- p>" << &p << endl;
+// };
+
+// int main()
+// {
+//     int x = 10;
+//     int *p = &x;
+//     fn(p);
+//     cout << "main--p>" << &p << endl;
+//     return 0;
+// }
+
+//! insertAtHead
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *next;
+
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+void insertAtHead(Node* &head, int newValue)
+{
+    Node *newNode = new Node(newValue);
+    newNode->next = head;
+    head = newNode;
+};
+
+void printLinkList(Node *head)
+{
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        /* code */
+        cout << temp->value << endl;
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+
+    head->next = a; // set pointer by next
+    a->next = b;
+    b->next = c;
+
+    insertAtHead(head, 152);
+    printLinkList(head);
+    return 0;
+}
