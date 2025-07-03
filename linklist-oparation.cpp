f4f9cdac2fc7269a71556df82b649fbbef6a8684
Tansim Ahmed Tashdid <tansimahmedtasjid@gmail.com>
2025-07-03 15:46:27 +0600

    insert head and tail both

--- linklist-oparation.cpp
+++ linklist-oparation.cpp
@@ -124,8 +124,70 @@
 //     return 0;
 // }
 
-//! insert tail
+// //! insert tail
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// class Node
+// {
+// public:
+//     int value;
+//     Node *next;
+
+//     Node(int value)
+//     {
+//         this->value = value;
+//         this->next = NULL;
+//     }
+// };
+
+// void insertTail(Node *&head, int newValue)
+// {
+//     Node *newNode = new Node(newValue);
+
+//     Node *temp = head;
+//     while (temp->next != NULL)
+//     {
+//         temp = temp->next;
+//     }
+//     // temp কে last node এ আনতে হবে । এখন last node এ আছে ।
+//     temp->next = newNode;
+// }
+
+// void printLinkList(Node *head)
+// {
+//     Node *temp = head;
+
+//     while (temp != NULL)
+//     {
+//         /* code */
+//         cout << temp->value << endl;
+//         temp = temp->next;
+//     }
+// }
+
+// int main()
+// {
+
+//     Node *head = new Node(10);
+//     Node *a = new Node(20);
+//     Node *b = new Node(30);
+//     Node *c = new Node(40);
+
+//     head->next = a; // set pointer by next
+//     a->next = b;
+//     b->next = c;
+
+//     insertTail(head, 69);
+//     insertTail(head, 49);
+//     printLinkList(head);
+//     return 0;
+// }
+
+
+
+//! insert head and tail both
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -142,23 +204,10 @@ public:
     }
 };
 
-void insertTail(Node *&head, int newValue)
+void printLinkList(Node *&head)
 {
-    Node *newNode = new Node(newValue);
 
     Node *temp = head;
-    while (temp->next != NULL)
-    {
-        temp = temp->next;
-    }
-    // temp কে last node এ আনতে হবে । এখন last node এ আছে । 
-    temp->next = newNode;
-}
-
-void printLinkList(Node *head)
-{
-    Node *temp = head;
-
     while (temp != NULL)
     {
         /* code */
@@ -167,20 +216,43 @@ void printLinkList(Node *head)
     }
 }
 
+
+void insertAtHead(Node* &head,int newValue){
+    Node* newNode = new Node(newValue);
+    newNode->next = head;
+    head = newNode;
+}
+void insertAtTail(Node* &head,int newValue){
+    Node* newNode = new Node(newValue);
+    Node* temp = head ;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    
+}
+
+
 int main()
 {
-
     Node *head = new Node(10);
     Node *a = new Node(20);
     Node *b = new Node(30);
-    Node *c = new Node(40);
 
-    head->next = a; // set pointer by next
+    head->next = a;
     a->next = b;
-    b->next = c;
 
-    insertTail(head, 69);
-    insertTail(head, 49);
+    // insert at head
+    insertAtHead(head,52);
+    insertAtHead(head,60);
+
+    // insert at tail
+    insertAtTail(head,80);
+    insertAtTail(head,90);
+
+    // print here
     printLinkList(head);
+
     return 0;
 }
