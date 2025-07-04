45e848f97bc44691da7e5ec5d301f39a0f1c7c94
Tansim Ahmed Tashdid <tansimahmedtasjid@gmail.com>
2025-07-04 23:29:38 +0600

    Create a singly linked list and print the middle element. If there are multiple values in the middle print both.

--- practice-1.linklist.cpp
+++ practice-1.linklist.cpp
@@ -47,69 +47,138 @@
 //     return 0;
 // }
 
+//! isfound value by link list
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// class Node
+// {
+// public:
+//     int value;
+//     Node *next;
+
+//     Node(int value)
+//     {
+//         this->value = value;
+//         this->next = NULL;
+//     }
+// };
+
+// void isFound(Node *head)
+// {
+//     Node *temp = head;
+//     bool isFoundValue = false;
+//     set<int> store;
+//     while (temp != NULL)
+//     {
+//         /* code */
+
+//         if (store.count(temp->value))
+//         {
+//             /* code */
+//             isFoundValue = true;
+//             break;
+//         }
+//         store.insert(temp->value);
+//         temp = temp->next;
+//     }
+
+//     cout << (isFoundValue ? "YES" : "NO");
+// }
+
+// int main()
+// {
+
+//     Node *head = new Node(10);
+//     Node *a = new Node(20);
+//     Node *b = new Node(30);
+//     Node *c = new Node(40);
+//     Node *d = new Node(90);
+//     Node *e = new Node(90);
+//     Node *f = new Node(90);
+
+//     head->next = a;
+//     a->next = b;
+//     b->next = c;
+//     c->next = d;
+//     d->next = e;
+//     e->next = f;
+
+//     isFound(head);
+
+//     return 0;
+// }
 
 
 
 
 
-//! isfound value by link list 
-#include <bits/stdc++.h>
-using namespace std;
+//! Create a singly linked list and print the middle element. If there are multiple values in the middle print both.
 
-class Node
-{
-public:
-    int value;
-    Node *next;
+// #include <bits/stdc++.h>
+// using namespace std;
 
-    Node(int value)
-    {
-        this->value = value;
-        this->next = NULL;
-    }
-};
+// class Node
+// {
+// public:
+//     int value;
+//     Node *next;
 
-void isFound(Node *head)
-{
-    Node *temp = head;
-    bool isFoundValue = false;
-    set<int> store;
-    while (temp != NULL)
-    {
-        /* code */
+//     Node(int value)
+//     {
+//         this->value = value;
+//         this->next = NULL;
+//     }
+// };
 
-        if (store.count(temp->value))
-        {
-            /* code */
-            isFoundValue = true;
-            break;
-        }
-        store.insert(temp->value);
-        temp = temp->next;
-    }
+// void printMiddleValue(Node *head)
+// {
+//     Node *temp = head;
+//     int count = 0;
+//     while (temp != NULL)
+//     {
+//         /* code */
+//         count++;
+//         temp = temp->next;
+//     }
 
-    cout << (isFoundValue ? "YES" : "NO");
-}
+//     int middle = count/2;
+//     Node *newTem = head;
+//     for (size_t i = 1; i < middle; i++)
+//     {
+//         /* code */
+//         newTem = newTem->next;
+//     }
 
-int main()
-{
+    
+ 
+//     if (count%2 != 0)
+//     {
+//         /* code */
+//         cout << newTem->next->value ;
+//     }else {
+//         cout << newTem->value  << " " << newTem->next->value;
+//     }
+    
+    
+    
+// }
 
-    Node *head = new Node(10);
-    Node *a = new Node(20);
-    Node *b = new Node(30);
-    Node *c = new Node(40);
-    Node *d = new Node(90);
-    Node *e = new Node(90);
-    Node *f = new Node(90);
+// int main()
+// {
 
-    head->next = a;
-    a->next = b;
-    b->next = c;
-    c->next = d;
-    d->next = e;
-    e->next = f;
+//     Node *head = new Node(1);
+//     Node *a = new Node(2);
+//     Node *b = new Node(3);
+//     Node *c = new Node(4);
+//     Node *d = new Node(5);
 
-    isFound(head);
+//     head->next = a;
+//     a->next = b;
+//     b->next = c;
+//     c->next = d;
 
-    return 0;
-}
+//     printMiddleValue(head);
+
+//     return 0;
+// }
