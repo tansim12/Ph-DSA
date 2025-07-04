1cf4c76873b3e18026d25c706e06377b9a265f0a
Tansim Ahmed Tashdid <tansimahmedtasjid@gmail.com>
2025-07-04 22:03:07 +0600

    input dynamic value

--- linklist-oparation.cpp
+++ linklist-oparation.cpp
@@ -267,6 +267,84 @@
 // }
 
 //! insert any where
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// class Node
+// {
+// public:
+//     int value;
+//     Node *next;
+
+//     Node(int value)
+//     {
+//         this->value = value;
+//         this->next = NULL;
+//     }
+// };
+
+// void printLinkList(Node *&head)
+// {
+
+//     Node *temp = head;
+//     while (temp != NULL)
+//     {
+//         /* code */
+//         cout << temp->value << endl;
+//         temp = temp->next;
+//     }
+// }
+
+// // insert any where
+// void insertByAny(Node *&head, int newValue, int index)
+// {
+//     Node *newNode = new Node(newValue);
+
+//     // check if linklist null or no value here
+//     if (head == NULL)
+//     {
+//         /* code */
+//         head = newNode;
+//         return;
+//     }
+
+//     Node *temp = head;
+//     for (size_t i = 0; i < index - 1; i++)
+//     {
+//         /* code */
+//         temp = temp->next;
+//     }
+//     // currently temp is index-1 position.
+//     // আগে লাস্টের node এর সাথে add করতে হবে তার পরে first এর টার সাথে add করতে হবে ।
+//     newNode->next = temp->next; // connect last
+//     temp->next = newNode;       // connect first
+// }
+
+// int main()
+// {
+//     // Node *head = NULL;
+//     Node *head = new Node(10);
+//     Node *a = new Node(20);
+//     Node *b = new Node(30);
+//     Node *c = new Node(40);
+
+//     head->next = a;
+//     a->next = b;
+//     b->next = c;
+
+//     // insert any
+//     insertByAny(head, 80, 2);
+//     insertByAny(head, 81, 4);
+
+//     // print here
+//     printLinkList(head);
+
+//     return 0;
+// }
+
+
+
+//! input value
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -295,12 +373,9 @@ void printLinkList(Node *&head)
     }
 }
 
-// insert any where
-void insertByAny(Node *&head, int newValue, int index)
+void insertAtTail(Node *&head, int newValue)
 {
     Node *newNode = new Node(newValue);
-
-    // check if linklist null or no value here
     if (head == NULL)
     {
         /* code */
@@ -309,32 +384,27 @@ void insertByAny(Node *&head, int newValue, int index)
     }
 
     Node *temp = head;
-    for (size_t i = 0; i < index - 1; i++)
+    while (temp->next != NULL)
     {
         /* code */
         temp = temp->next;
     }
-    // currently temp is index-1 position.
-    // আগে লাস্টের node এর সাথে add করতে হবে তার পরে first এর টার সাথে add করতে হবে ।
-    newNode->next = temp->next; // connect last
-    temp->next = newNode;       // connect first
+    temp->next = newNode;
 }
 
 int main()
 {
-    // Node *head = NULL;
-    Node *head = new Node(10);
-    Node *a = new Node(20);
-    Node *b = new Node(30);
-    Node *c = new Node(40);
+    int n;
+    cin >> n;
+    Node *head = NULL;
 
-    head->next = a;
-    a->next = b;
-    b->next = c;
-
-    // insert any
-    insertByAny(head, 80, 2);
-
+    for (size_t i = 0; i < n; i++)
+    {
+        /* code */
+        int newValue;
+        cin >> newValue;
+        insertAtTail(head, newValue);
+    }
     // print here
     printLinkList(head);
 
