d3f1bde3e596c7678091bb3eef71a963f837c325
Tansim Ahmed Tashdid <tansimahmedtasjid@gmail.com>
2025-07-05 12:12:24 +0600

    delete at any position

--- more-oparation.linklist.cpp
+++ more-oparation.linklist.cpp
@@ -65,8 +65,74 @@
 //     return 0;
 // }
 
-
 //! reverse print with recursion
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// class Node
+// {
+// public:
+//     int value;
+//     Node *next;
+
+//     Node(int value)
+//     {
+//         this->value = value;
+//         this->next = NULL;
+//     }
+// };
+
+// void printLinkList(Node *&head)
+// {
+//     Node *temp = head;
+//      if (temp == NULL)
+//     {
+//         /* code */
+//         return;
+//     }
+//     cout << temp->value << endl;
+//     printLinkList(temp->next);
+// }
+
+// void insertAtTail(Node *&head, Node *&tail, int newValue)
+// {
+//     Node *newNode = new Node(newValue);
+//     if (head == NULL)
+//     {
+//         /* code */
+//         head = newNode;
+//         tail = newNode;
+//         return;
+//     }
+
+//     tail->next = newNode;
+//     tail = newNode;
+// }
+
+// int main()
+// {
+//     Node *head = NULL;
+//     Node *tail = NULL;
+
+//     int val;
+//     while (true)
+//     {
+//         /* code */
+//         cin >> val;
+//         if (val == -1)
+//         {
+//             /* code */
+//             break;
+//         }
+//         insertAtTail(head, tail, val);
+//     }
+
+//     printLinkList(head);
+
+//     return 0;
+// }
+
+//! delete head
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -86,13 +152,13 @@ public:
 void printLinkList(Node *&head)
 {
     Node *temp = head;
-     if (temp == NULL)
+    if (temp == NULL)
     {
         /* code */
         return;
     }
-    printLinkList(temp->next);
     cout << temp->value << endl;
+    printLinkList(temp->next);
 }
 
 void insertAtTail(Node *&head, Node *&tail, int newValue)
@@ -108,6 +174,31 @@ void insertAtTail(Node *&head, Node *&tail, int newValue)
 
     tail->next = newNode;
     tail = newNode;
+}
+
+void deleteHead(Node *&head)
+{
+    Node *temp = head;
+    head = head->next;
+    delete temp;
+}
+void deleteAtAny(Node *&head, int position)
+{
+    Node *temp = head;
+    for (size_t i = 1; i < position; i++)
+    {
+        /* code */
+        if (temp == NULL)
+        {
+            /* code */
+            return;
+        }
+        temp = temp->next;
+    }
+    Node *deleteNode = temp->next;
+    temp->next = deleteNode->next;
+    delete deleteNode;
+
 }
 
 int main()
@@ -127,7 +218,14 @@ int main()
         }
         insertAtTail(head, tail, val);
     }
+    //! delete head
+    // deleteHead(head);
 
+
+    //! delete at any 
+    deleteAtAny(head,3);
+
+    //! Print with recursion 
     printLinkList(head);
 
     return 0;
