aabe042bfd75282714291539d3dea6fad48d44bc
Tansim Ahmed Tashdid <tansimahmedtasjid@gmail.com>
2025-07-11 16:27:09 +0600

    delete head, delete tail , delete any

--- mod-9.doublylinklist.cpp
+++ mod-9.doublylinklist.cpp
@@ -110,13 +110,95 @@ void insertAtAny(Node *&head, Node *&tail, int position, int newValue)
             /* code */
             return;
         }
-
     }
 
     newNode->next = temp->next;
     temp->next->prev = newNode;
     newNode->prev = temp;
     temp->next = newNode;
+}
+
+// delete at head
+void deleteAtHead(Node *&head, Node *&tail)
+{
+    if (head == NULL)
+    {
+        return;
+    }
+    Node *temp = head;
+    if (temp->next == NULL)
+    {
+        /* code */
+        head = NULL;
+        tail = NULL;
+        delete temp;
+        return;
+    }
+
+    temp->next->prev = NULL;
+    head = temp->next;
+    delete temp;
+}
+
+// delete at tail
+void deleteAtTail(Node *&head, Node *&tail)
+{
+    if (head == NULL)
+    {
+        return;
+    }
+    Node *temp = tail;
+
+    if (temp->prev == NULL)
+    {
+        /* code */
+        head = NULL;
+        tail = NULL;
+        delete temp;
+        return;
+    }
+
+    temp->prev->next = NULL;
+    tail = temp->prev;
+    delete temp;
+}
+
+// delete at any position
+//! But if position was tail then deleteAtAny not working (array length যদি 3 hoy তাইলে 3 position এ delete করা যাবে না)
+void deleteAtAny(Node *&head, Node *&tail, int position)
+{
+
+    if (head == NULL)
+    {
+        return;
+    }
+
+    Node *temp = head;
+
+    if (position == 0)
+    {
+        /* code */
+        Node *temp = head;
+        temp->next->prev = NULL;
+        head = temp->next;
+        delete temp;
+        return;
+    }
+
+    for (size_t i = 0; i < position; i++)
+    {
+        /* code */
+        temp = temp->next;
+        if (temp == NULL)
+        {
+            /* code */
+            return;
+        }
+    }
+
+    temp->next->prev = temp->prev;
+    temp->prev->next = temp->next;
+    delete temp;
 }
 
 int main()
@@ -139,9 +221,18 @@ int main()
     }
 
     // insert At any
-    insertAtAny(head, tail, 6, 96);
+    // insertAtAny(head, tail, 6, 96);
+
+    // delete At head
+    // deleteAtHead(head, tail);
+
+    // delete At tail
+    // deleteAtTail(head, tail);
+
+    // delete At any
+    deleteAtAny(head, tail,1);
 
     printForword(head);
-    printBackword(tail);
+    // printBackword(tail);
     return 0;
 }
