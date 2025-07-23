8393598d08f56bed5584d7123908fc7d901e6636
Tansim Ahmed Tashdid <tansimahmedtasjid@gmail.com>
2025-07-23 20:31:09 +0600

    using stack STL

--- stack.cpp
+++ stack.cpp
@@ -44,6 +44,57 @@
 //         st.push(val);
 //     }
 
+//     while(!st.empty()){
+//         cout << st.top() << endl ;
+//         st.pop();
+//     }
+
+//     return 0;
+// }
+
+//! using list
+// #include <bits/stdc++.h>
+// using namespace std;
+// class MyStack
+// {
+// public:
+//     list<int> l;
+
+//     void push(int val)
+//     {
+//         l.push_back(val);
+//     };
+//     void pop()
+//     {
+//         l.pop_back();
+//     };
+//     int top()
+//     {
+//         return l.back();
+//     };
+//     int size()
+//     {
+//         return l.size();
+//     };
+//     bool empty()
+//     {
+//         return l.empty();
+//     };
+// };
+
+// int main()
+// {
+
+//     MyStack st;
+//     int n;
+//     cin >> n;
+//     for (size_t i = 0; i < n; i++)
+//     {
+//         /* code */
+//         int val;
+//         cin >> val;
+//         st.push(val);
+//     }
 
 //     while(!st.empty()){
 //         cout << st.top() << endl ;
@@ -55,41 +106,14 @@
 
 
 
-
-//! using list
+//! using stack stl
 #include <bits/stdc++.h>
 using namespace std;
-class MyStack
-{
-public:
-    list<int> l;
-
-    void push(int val)
-    {
-        l.push_back(val);
-    };
-    void pop()
-    {
-        l.pop_back();
-    };
-    int top()
-    {
-        return l.back();
-    };
-    int size()
-    {
-        return l.size();
-    };
-    bool empty()
-    {
-        return l.empty();
-    };
-};
 
 int main()
 {
 
-    MyStack st;
+    stack<int> st;
     int n;
     cin >> n;
     for (size_t i = 0; i < n; i++)
@@ -100,9 +124,9 @@ int main()
         st.push(val);
     }
 
-
-    while(!st.empty()){
-        cout << st.top() << endl ;
+    while (!st.empty())
+    {
+        cout << st.top() << endl;
         st.pop();
     }
 
