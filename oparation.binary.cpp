ca7994fd6261fe36ceb65e90e1f372e72ce583ed
Tansim Ahmed Tashdid <tansimahmedtasjid@gmail.com>
2025-08-04 22:35:48 +0600

    count total node and leaf node

--- oparation.binary.cpp
+++ oparation.binary.cpp
@@ -69,7 +69,109 @@
 //     return 0;
 // }
 
-//! input tree
+// //! input tree
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// class Node
+// {
+// public:
+//     int value;
+//     Node *left;
+//     Node *right;
+
+//     Node(int value)
+//     {
+//         this->value = value;
+//         this->left = NULL;
+//         this->right = NULL;
+//     }
+// };
+
+// void levelOrderPrint(Node *root)
+// {
+//     queue<Node *> q;
+
+//     q.push(root);
+//     while (!q.empty())
+//     {
+//         /* code */
+//         //* ১। বের করে আনলাম ।
+//         Node *frontValue = q.front();
+//         q.pop();
+
+//         //* ২। ঐ node নিয়ে কাজ করলাম  ।
+//         cout << frontValue->value << " ";
+
+//         //* ৩। ঐ node এর children গুলা push করলাম  ।
+//         if (frontValue->left)
+//         {
+//             /* code */
+//             q.push(frontValue->left);
+//         }
+//         if (frontValue->right)
+//         {
+//             /* code */
+//             q.push(frontValue->right);
+//         }
+//     }
+// }
+
+// Node *inputTree()
+// {
+//     int val;
+//     cin >> val;
+//     Node *root = new Node(val);
+//     if (val == -1)
+//         root = NULL;
+//     queue<Node *> q;
+//     if (root)
+//         q.push(root);
+
+//     while (!q.empty())
+//     {
+//         /* code */
+//         //* 1. ber kore ana
+//         Node *frontNode = q.front();
+//         q.pop();
+
+//         //* 2. kaj kora
+//         int l, r;
+//         cin >> l >> r;
+//         Node *newL, *newR;
+
+//         if (l == -1)
+//             newL = NULL;
+//         else
+//             newL = new Node(l);
+
+//         if (r == -1)
+//             newR = NULL;
+//         else
+//             newR = new Node(r);
+
+//         frontNode->left = newL;
+//         frontNode->right = newR;
+
+//         //* 3. child push kora
+//         if (frontNode->left)
+//             q.push(frontNode->left);
+//         if (frontNode->right)
+//             q.push(frontNode->right);
+//     }
+//     return root;
+// }
+
+// int main()
+// {
+
+//     Node *root = inputTree();
+//     levelOrderPrint(root);
+
+//     return 0;
+// }
+
+//! print node count, leaf node count by recursion
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -88,33 +190,69 @@ public:
     }
 };
 
-void levelOrderPrint(Node *root)
+//* node count by level wise
+// int countNode(Node *root)
+// {
+//     int cnt = 0;
+//     if (root == NULL)
+//     {
+//         /* code */
+//         return 0;
+//     }
+
+//     queue<Node *> q;
+//     q.push(root);
+
+//     while (!q.empty())
+//     {
+//         /* code */
+//         Node *frontValue = q.front();
+//         q.pop();
+
+//         cnt++;
+
+//         if (frontValue->left)
+//         {
+//             /* code */
+//             q.push(frontValue->left);
+//         }
+//         if (frontValue->right)
+//         {
+//             /* code */
+//             q.push(frontValue->right);
+//         }
+//     }
+
+//     return cnt;
+// }
+
+//* count by recursion
+// int countNode(Node *root)
+// {
+//     if (!root)
+//         return 0;
+
+//     int totalLeftNode = countNode(root->left);
+//     int totalRightNode = countNode(root->right);
+
+//     return (totalLeftNode + totalRightNode);
+// }
+
+
+//* count leaf node by recursion
+//* count by recursion
+int countLeafNodeNode(Node *root)
 {
-    queue<Node *> q;
+    if (!root)
+        return 0;
 
-    q.push(root);
-    while (!q.empty())
-    {
-        /* code */
-        //* ১। বের করে আনলাম ।
-        Node *frontValue = q.front();
-        q.pop();
+    if (root->left == NULL && root->right == NULL)
+        return 1;
 
-        //* ২। ঐ node নিয়ে কাজ করলাম  ।
-        cout << frontValue->value << " ";
+    int totalLeftNode = countLeafNodeNode(root->left);
+    int totalRightNode = countLeafNodeNode(root->right);
 
-        //* ৩। ঐ node এর children গুলা push করলাম  ।
-        if (frontValue->left)
-        {
-            /* code */
-            q.push(frontValue->left);
-        }
-        if (frontValue->right)
-        {
-            /* code */
-            q.push(frontValue->right);
-        }
-    }
+    return (totalLeftNode + totalRightNode);
 }
 
 Node *inputTree()
@@ -166,7 +304,9 @@ int main()
 {
 
     Node *root = inputTree();
-    levelOrderPrint(root);
+
+    // cout << countNode(root);
+    cout << countLeafNodeNode(root);
 
     return 0;
 }
