4b2fd798f093f71152f38a709f562508bd9ffc5d
Tansim Ahmed Tashdid <tansimahmedtasjid@gmail.com>
2025-08-19 14:03:25 +0600

    bfs print

---
+++ bfs.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int>adj_list[10005];
+bool vst[10005];
+
+void bfs(int src){
+    queue<int>q;
+    q.push(src);
+    vst[src] = true;
+
+    while (!q.empty())
+    {
+        /* code */
+        int parent = q.front();
+        q.pop();
+
+        cout << parent << " ";
+
+        // children push
+        for (auto &&i : adj_list[parent])
+        {
+            if(vst[i] == false){
+                q.push(i);
+                vst[i] = true;
+            }
+        }
+        
+
+    }
+    
+}
+
+int main() {
+    int n,e;
+    cin >> n >> e;
+    while (e--)
+    {
+        /* code */
+        int a,b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+
+    }
+    
+    memset(vst,false,sizeof(vst));
+    bfs(0);
+    
+    return 0;
+}
