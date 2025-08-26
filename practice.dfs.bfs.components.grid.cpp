1db4a0efecc33f899802234cbbc36a86575b2ccf
Tansim Ahmed Tashdid <tansimahmedtasjid@gmail.com>
2025-08-26 13:06:39 +0600

    You will be given an undirected graph as input. You need to tell the number of components in this graph.

--- practice.dfs.bfs.components.grid.cpp
+++ practice.dfs.bfs.components.grid.cpp
@@ -1,4 +1,50 @@
-//  You will be given an undirected graph as input. Then you will be given a node N. You need to tell the number of nodes that can be visited from node N.
+// ! You will be given an undirected graph as input. Then you will be given a node N. You need to tell the number of nodes that can be visited from node N.
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// vector<int> adj_list[105];
+// bool visited[105];
+// int cnt = 0;
+
+// void dfs(int src)
+// {
+//     cnt++;
+//     visited[src] = true;
+//     for (auto &&child : adj_list[src])
+//     {
+//         if (!visited[child])
+//         {
+//             dfs(child);
+//             visited[child] = true;
+//         }
+//     }
+// }
+
+// int main()
+// {
+//     int n, e;
+//     cin >> n >> e;
+//     while (e--)
+//     {
+//         /* code */
+//         int a, b;
+//         cin >> a >> b;
+//         adj_list[a].push_back(b);
+//         adj_list[b].push_back(a);
+//     }
+//     memset(visited, false, sizeof(visited));
+
+//     int src;
+//     cin >> src;
+
+//     dfs(src);
+
+//     cout << cnt;
+
+//     return 0;
+// }
+
+//! You will be given an undirected graph as input. You need to tell the number of components in this graph.
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -8,7 +54,6 @@ int cnt = 0;
 
 void dfs(int src)
 {
-    cnt++;
     visited[src] = true;
     for (auto &&child : adj_list[src])
     {
@@ -33,6 +78,16 @@ int main()
         adj_list[b].push_back(a);
     }
     memset(visited, false, sizeof(visited));
+
+    for (size_t i = 0; i < n; i++)
+    {
+        /* code */
+        if (!visited[i])
+        {
+            dfs(i);
+            cnt++;
+        }
+    }
 
     int src;
     cin >> src;
