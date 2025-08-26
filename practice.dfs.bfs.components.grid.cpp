038544ec0f29348c4e9c2622d39064a77241770d
Tansim Ahmed Tashdid <tansimahmedtasjid@gmail.com>
2025-08-26 13:03:29 +0600

    You will be given an undirected graph as input. Then you will be given a node N. You need to tell the number of nodes that can be visited from node N.

---
+++ practice.dfs.bfs.components.grid.cpp
@@ -0,0 +1,45 @@
+//  You will be given an undirected graph as input. Then you will be given a node N. You need to tell the number of nodes that can be visited from node N.
+
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> adj_list[105];
+bool visited[105];
+int cnt = 0;
+
+void dfs(int src)
+{
+    cnt++;
+    visited[src] = true;
+    for (auto &&child : adj_list[src])
+    {
+        if (!visited[child])
+        {
+            dfs(child);
+            visited[child] = true;
+        }
+    }
+}
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    while (e--)
+    {
+        /* code */
+        int a, b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+    memset(visited, false, sizeof(visited));
+
+    int src;
+    cin >> src;
+
+    dfs(src);
+
+    cout << cnt;
+
+    return 0;
+}
