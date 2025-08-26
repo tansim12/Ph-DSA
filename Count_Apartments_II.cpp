1f51cf3620de2c05ea17f62d95613b80eeb62380
Tansim Ahmed Tashdid <tansimahmedtasjid@gmail.com>
2025-08-26 16:15:37 +0600

    Count Apartments 2

---
+++ Count_Apartments_II.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+int r, c;
+char grid[1005][1005];
+bool visited[1005][1005];
+vector<pair<int, int>> d = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+// check this node is valid
+bool isValid(int i, int j)
+{
+    if (i < 0 || i >= r || j < 0 || j >= c)
+        return false;
+
+    return true;
+}
+
+int dfs(int si, int sj)
+{
+    visited[si][sj] = true;
+    int ct = 1;
+    for (size_t i = 0; i < 4; i++)
+    {
+        /* code */
+        int ci, cj;
+        ci = si + d[i].first;
+        cj = sj + d[i].second;
+
+       if (isValid(ci, cj) && !visited[ci][cj] && grid[ci][cj] == '.')
+        {
+          ct+=dfs(ci, cj);
+            
+        }
+    }
+    return ct;
+}
+
+int main()
+{
+
+    cin >> r >> c;
+
+    for (size_t i = 0; i < r; i++)
+    {
+        /* code */
+        for (size_t j = 0; j < c; j++)
+        {
+            /* code */
+            cin >> grid[i][j];
+        }
+    }
+
+
+    memset(visited, false, sizeof(visited));
+    vector<int>v;
+      for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            if (!visited[i][j] && grid[i][j] == '.')
+            {
+              int cnt= dfs(i, j);
+              v.push_back(cnt);
+            }
+        }
+    }
+
+     if (v.empty()) {
+        cout << endl;
+        return 0;
+    }
+
+    sort(v.begin(),v.end());
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i];
+        if (i + 1 < v.size()) cout << " ";
+    }
+    cout << endl;
+    
+
+    return 0;
+}
