11868397ed5ebd9c4596520f55d22318cfcb2668
Tansim Ahmed Tashdid <tansimahmedtasjid@gmail.com>
2025-09-02 14:40:04 +0600

    sssp with dijkstra

---
+++ naive.dijkstra.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<pair<int, int>> adj_list[105];
+int dist[105];
+
+void dijkstra(int src)
+{
+    queue<pair<int, int>> q;
+    q.push({src, 0});
+    dist[src] = 0 ;
+
+    while (!q.empty())
+    {
+        /* code */
+        pair<int, int> parent = q.front();
+        q.pop();
+
+        int parent_node = parent.first;
+        int parent_dist = parent.second;
+
+        // children push
+        for (auto &&child : adj_list[parent_node])
+        {
+            int child_node = child.first;
+            int child_dist = child.second;
+            if (parent_dist + child_dist < dist[child_node])
+            {
+                dist[child_node] = parent_dist + child_dist;
+                q.push({child_node, dist[child_node]});
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    while (e--)
+    {
+        /* code */
+        int a, b, c;
+        cin >> a >> b >> c;
+        adj_list[a].push_back({b, c});
+        adj_list[b].push_back({a, c});
+    }
+
+    for (size_t i = 0; i < n; i++)
+    {
+        /* code */
+        dist[i] = INT_MAX;
+    }
+
+    dijkstra(0);
+
+   for (size_t i = 0; i < n; i++)
+    {
+        /* code */
+       cout << i << " Node-> " << dist[i] << endl ;
+    }
+
+    return 0;
+}
